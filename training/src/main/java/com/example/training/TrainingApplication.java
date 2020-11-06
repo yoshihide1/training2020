4d5375c35ce47c12c2aa4214b36d275f8c9afd3a
tsukamoto <tsukamoto@tierline.com>
2020-11-06 15:08:28 +0900

    header部分サンプル作成

--- training/src/main/java/com/example/training/TrainingApplication.java
+++ training/src/main/java/com/example/training/TrainingApplication.java
@@ -8,6 +8,7 @@ public class TrainingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingApplication.class, args);
+		System.out.println("Hello World!!");
 	}
 
 }
