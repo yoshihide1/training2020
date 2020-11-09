34f4b30d5b339592c69b93f5fa06795374f313f3
tsukamoto <tsukamoto@tierline.com>
2020-11-09 17:15:32 +0900

    画面遷移できるように仮のリンク設置

--- training/src/main/java/com/example/training/MainController.java
+++ training/src/main/java/com/example/training/MainController.java
@@ -3,24 +3,25 @@ package com.example.training;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class MainController {
 	@GetMapping("/index")
 	public String index(Model model) {
-		//model.addAttribute("message", "Hello");
+		// model.addAttribute("message", "Hello");
 		return "index";
 	}
 
 	@GetMapping("/cart")
 	public String cart(Model model) {
-		//model.addAttribute("message", "Hello");
+		// model.addAttribute("message", "Hello");
 		return "cart";
 	}
 
-	@GetMapping("/cart")
+	@GetMapping("/product")
 	public String product(Model model) {
-		//model.addAttribute("message", "Hello");
-		return "puroduct";
+		// model.addAttribute("message", "Hello");
+		return "product";
 	}
 
 }
