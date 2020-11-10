fe94a6d14cb08b405e85a11cddc3c0ad3e06d527
takeshitaRin <takeshita@tierline.com>
2020-11-10 17:10:14 +0900

    insert csv

--- training/db/init.sh
+++ training/db/init.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+cd db
+
+echo -------create table--------
+
 mysql --defaults-extra-file=root.conf < createTable.txt
+
+echo ------- finish--------
+
+echo -------create table--------
+
+mysql --defaults-extra-file=root.conf < insert_product.txt
+
+echo ------- finish--------
