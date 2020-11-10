ddbe33b1b6e1da91944a0118d6b90835061a9eb1
takeshitaRin <takeshita@tierline.com>
2020-11-10 19:03:56 +0900

    add database and change .sh

--- training/db/init.sh
+++ training/db/init.sh
@@ -11,7 +11,10 @@ echo ------- finish--------
 echo -------create table--------
 
 mysql --defaults-extra-file=root.conf --enable-local-infile < ./insert_product.txt
+<<<<<<< HEAD
 
 echo ------- finish--------
 
+=======
+>>>>>>> e5c9356... add database and change .sh
 
