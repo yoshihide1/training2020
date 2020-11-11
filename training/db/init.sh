69e4a8683cc2ec7bd0f03b5ef2721c2e64349836
takeshitaRin <takeshita@tierline.com>
2020-11-11 13:25:51 +0900

    .txt rename and modified init.sh

--- training/db/init.sh
+++ training/db/init.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-cd db
 
 echo -------create table--------
 
-mysql --defaults-extra-file=root.conf < createTable.txt
+mysql --defaults-extra-file=root.conf < createTable.sql
 
 echo ------- finish--------
 
 echo -------create table--------
 
 mysql --defaults-extra-file=root.conf --enable-local-infile < ./insert_product.txt
-<<<<<<< HEAD
 
 echo ------- finish--------
-
-=======
->>>>>>> e5c9356... add database and change .sh
-
