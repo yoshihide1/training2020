50a6bf7bdcfe073c0599f6e50a13d78a17d6d3ce
tsukamoto <tsukamoto@tierline.com>
2020-11-11 12:37:09 +0900

    mapper 作成

--- training/db/init.sh
+++ training/db/init.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 mysql --defaults-extra-file=root.conf < createTable.txt
-<<<<<<< HEAD
-=======
 
 echo ------- finish--------
 
@@ -11,4 +9,4 @@ echo -------create table--------
 mysql --defaults-extra-file=root.conf --enable-local-infile < ./insert_product.txt
 
 echo ------- finish--------
->>>>>>> e5c9356 (add database and change .sh)
+
